b5400c8e6fd7dede9efe049ed82db127eafbd053
Andy Meneely <andy@se.rit.edu>
2016-10-30 03:39:03 -0400

    more gsubs just for fun

--- lib/l33t_words.rb
+++ lib/l33t_words.rb
@@ -3,14 +3,16 @@ module XohImNooB
 
     def self.make(word)
       l33t_word = word.
-                    gsub('to',  '2').
-                    gsub('for', '4').
-                    gsub('ate', '8').
-                    gsub('l',   '1').
-                    gsub('e',   '3').
-                    gsub('s',   '5').
-                    gsub('t',   '7').
-                    gsub('o',   '0')
+                    gsub('to',    '2').
+                    gsub('for',   '4').
+                    gsub('ate',   '8').
+                    gsub('slash', '/').
+                    gsub('plus',  '+').
+                    gsub('l',     '1').
+                    gsub('e',     '3').
+                    gsub('s',     '5').
+                    gsub('t',     '7').
+                    gsub('o',     '0')
       return word == l33t_word ? [] : [l33t_word]
     end
 
