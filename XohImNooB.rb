d88a00d46838a74680f001d496c1d5ea71d5dcd6
Andy Meneely <andy@se.rit.edu>
2017-10-04 23:58:22 -0400

    Now with more XKCD power!

--- XohImNooB.rb
+++ XohImNooB.rb
@@ -17,20 +17,24 @@ puts "  ...sorting"
 @awesomeness = XohImNooB::Awesomeness.new(@words)
 puts "Done! #{@words.size} words in dictionary"
 
-puts "Looking for awesome hashes @ #{Time.now}..."
-@words.each do |word|
-# @words.sample(10000).each do |word| # choose some at random
+n = 274928
+puts "Sampling awesomeness in #{n} hashes @ #{Time.now}..."
+@words.sample(n).each do |word| # choose some at random	
   digest = Digest::SHA256.new.base64digest(word)
   awe, sub_str, word_list = @awesomeness.of(digest)
   if awe > 9
-    # digest = XohImNooB::UTFHighlight.go(digest, sub_str)
+    digest = XohImNooB::UTFHighlight.go(digest, sub_str)
     puts "base64(sha256(#{word})) = #{digest} (#{word_list.join(' ')})@ #{awe}"
-  end
+  end       
 end
 
-puts "Done! #{Time.now}"
+puts "Done! #{Time.now}."
 
 # Some good examples
 # base64(sha256(acceding)) = 9aRghAa3h4GLpQsWtCQonVy50l6ZbqLw2gEvADDg8uE= --> aRghAa (arghaa)@ 6
 # base64(sha256(acentrics)) = DENiUh0szFvQSfCq7j0S75pBROMCoM/w4bF4Ph1/KO4= --> ROMCoM (romcom)@ 6
 # base64(sha256(actinometries)) = zILNIAhl0H+ZZIJ0vahUVLoLigoRs/0D/cY3akW00js= --> LoLigo (loligo)@ 6
+# base64(sha256(yet)) = z9lt2S9+ACWxRhqvwr8K6upPebJ7GT9Jxh̭M̭s̭ṰG̭O̭j̭A̭M̭s̭= (hm st go jams)@ 10
+# base64(sha256(audiometrically)) = WkM9TkCcsYATsMnsuILRICibR̭ḘC̭K̭b̭i̭b̭r̭ḘA̭m̭i0c7W0eA= (reck bib ream)@ 11
+# base64(sha256(throbber)) = ILXfḓi̭m̭A̭ṰṱṰh̭A̭ṱy6lV5i9xBn1dxbzjn+wmsR7XtD2io= (di matt that)
+# base64(sha256(beauteously)) = R8jw2rMṊo̭B̭g̭I̭F̭ṰS̭ṋo̭ujJZbA3oNvj3NEDNJ8o2rSACY4= (nob gifts no)@ 10
--- lib/awesomeness.rb
+++ lib/awesomeness.rb
@@ -1,21 +1,22 @@
+require_relative 'xkcd_words'
+
 module XohImNooB
   class Awesomeness
     def initialize(words)
       @words = words
+      @xkcd_words = XKCD.words.sort
     end
 
     def of(digest)
       digest_downcase = digest.downcase
       best_word_list = []
-      @words.each do |word|
-        if digest_downcase.include? word
+      @words.each do |word|                # 1. Loop over all words in dict
+        if digest_downcase.include? word   # 2. If digest has the word
           word_list = [word]
           i = digest_downcase.index(word) + word.size
-          rest = digest_downcase[i..-1]
-          word_list += finish_phrase(rest)
-          if num_letters(word_list) > num_letters(best_word_list)
-            best_word_list = word_list
-          end
+          rest = digest_downcase[i..-1]    #    ...keep looking for more words
+          word_list += finish_phrase(rest) #    ...in an expensive recursive call
+          best_word_list = word_list if score(word_list) > score(best_word_list) 
         end
       end
       return [0, nil] if best_word_list.empty?
@@ -25,21 +26,27 @@ module XohImNooB
       return n, sub_str, best_word_list
     end
 
+    # My scoring system for awesomeness of a given word lists
+    def score(word_list)
+      word_list.inject(0) do |num, w|
+        num + 
+        w.size + # Overall letters in the list is good
+        w.size + # Longer word that get counted more with this second one
+        (is_xkcd?(w) ? w.size*w.size : 0) # Long XKCD words (top 1k) are best
+      end
+    end
+
+    # Count the number of letters in an array of words
     def num_letters(word_list)
       word_list.inject(0) { |num, w| num + w.size }
     end
 
+    # Recursively determine if we can find more and more words in the phrase
+    # This is pretty greedy and slow, but whatever.
     def finish_phrase(digest_tail, word_list = [])
       return word_list if digest_tail.empty?
       digest_tail.size.downto(1).each do |i|
         sub_str = digest_tail[0..i-1]
-        if digest_tail.start_with? 'noob'
-          if sub_str.eql? 'noob'
-            # require 'byebug'
-            # byebug
-          end
-        end
-
         if found(sub_str)
           word_list << sub_str
           return finish_phrase(digest_tail[i..-1], word_list)
@@ -48,8 +55,14 @@ module XohImNooB
       return word_list
     end
 
+    # Do a binary search on the giant word dictionary - should be fast
     def found(sub_str)
       @words.bsearch { |w| w >= sub_str } == sub_str
+    end
+
+    # Do a binary search on the XKCD top 1000 dictionary - should be fast
+    def is_xkcd?(word)
+      @xkcd_words.bsearch { |w| w >= word } == word
     end
 
   end
