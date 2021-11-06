6386816d94ca8a79e42e1ba734e25c8c3b7d5b3f
Andy Meneely <andy@se.rit.edu>
2021-11-06 00:37:33 -0400

    Getting going!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod xkcd_words;
+use sha2::{Sha256, Digest};
+
+pub fn xoh_hash(s: &String) -> String {
+    return base64::encode(Sha256::digest(s.as_bytes()));
+}
+
 fn main() {
-    println!("Hello, world!");
+    xkcd_words::load_words();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hash_works(){
+        assert_eq!(String::from("XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg="),
+                    xoh_hash(&String::from("password")))
+    }
 }
